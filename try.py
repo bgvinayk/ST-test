f16b9a3e030ad3de5ff298c80f9cd02fd990b3b5
bgvinayk <101205129+bgvinayk@users.noreply.github.com>
2022-07-14 22:54:40 +0530

    Update try.py

--- try.py
+++ try.py
@@ -1,5 +1,9 @@
 import streamlit as st
+import pandas as pd
 
 st.title("Cooling Water Program Recommendation")
 st.markdown("## Enter cooling tower parameters")
 st.subheader("MECHANICAL CONDITIONS")
+
+
+costProducts = pd.read_csv('https://github.com/bgvinayk/ST-test/blob/main/Model_Product_Costs.csv')
