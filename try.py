590d5b510a98ceadd93cd4db396b14ae94235013
bgvinayk <101205129+bgvinayk@users.noreply.github.com>
2022-07-14 23:32:47 +0530

    Update try.py

--- try.py
+++ try.py
@@ -6,6 +6,6 @@ st.markdown("## Enter cooling tower parameters")
 st.subheader("MECHANICAL CONDITIONS")
 
 
-costProducts = pd.read_csv('https://raw.githubusercontent.com/bgvinayk/ST-test/main/try.py')
+costProducts = pd.read_csv('https://raw.githubusercontent.com/bgvinayk/ST-test/main/try.py',index=0)
 
 st.subheader(costProducts[0][0])
