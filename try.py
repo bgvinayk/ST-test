e5c0f017ebf1f4c468b416a5f2e57a1d1cfeba03
bgvinayk <101205129+bgvinayk@users.noreply.github.com>
2022-07-17 16:02:53 +0530

    Update try.py

--- try.py
+++ try.py
@@ -3,11 +3,11 @@ import pandas as pd
 from scipy.optimize import fsolve
 from reaktoro import *
 
+db = PhreeqcDatabase("minteq.v4.dat")
+
 st.title("Cooling Water Program Recommendation")
 st.markdown("## Enter cooling tower parameters")
 st.subheader("MECHANICAL CONDITIONS")
-
-db = PhreeqcDatabase("minteq.v4.dat")
   
 costProducts = pd.read_csv('https://raw.githubusercontent.com/bgvinayk/ST-test/main/Model_Product_Costs.csv')
 
