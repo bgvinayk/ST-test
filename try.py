a577bc8b450b84a7ebb8c3b6e4c468d04be36db0
bgvinayk <101205129+bgvinayk@users.noreply.github.com>
2022-07-17 16:30:16 +0530

    Update try.py

--- try.py
+++ try.py
@@ -3,7 +3,7 @@ import pandas as pd
 from scipy.optimize import fsolve
 from reaktoro import *
 
-db = PhreeqcDatabase("minteq.v4.dat")
+# db = PhreeqcDatabase("minteq.v4.dat")
 
 st.title("Cooling Water Program Recommendation")
 st.markdown("## Enter cooling tower parameters")
