6c913e1fae9d5b4b19d753851282960e70cca3ee
bgvinayk <101205129+bgvinayk@users.noreply.github.com>
2022-07-17 15:54:18 +0530

    Update try.py

--- try.py
+++ try.py
@@ -1,13 +1,14 @@
 import streamlit as st
 import pandas as pd
 from scipy.optimize import fsolve
-from reaktoro import *
+import reaktoro as rkt
 
 st.title("Cooling Water Program Recommendation")
 st.markdown("## Enter cooling tower parameters")
 st.subheader("MECHANICAL CONDITIONS")
 
-db = PhreeqcDatabase("minteq.v4.dat")
+db1 = rkt.ThermoFunDatabase("slop98")
+db = rkt.PhreeqcDatabase("minteq.v4.dat")
   
 costProducts = pd.read_csv('https://raw.githubusercontent.com/bgvinayk/ST-test/main/Model_Product_Costs.csv')
 
